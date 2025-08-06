76d0665a116d6ffe7d9f4ca0e3fd859cc79e5413
Niponpal <niponpal52@gmail.com>
2025-08-06 12:48:09 +0600

    Added Dashboard

---
+++ BlogNestS/Areas/Admin/Controllers/BlogPostController.cs
@@ -0,0 +1,72 @@
+﻿using BlogNestS.Models;
+using BlogNestS.Repositorys;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlogNestS.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class BlogPostController : Controller
+    {
+        private readonly IBlogPostRepository _blogPostRepository;
+        public BlogPostController(IBlogPostRepository blogPostRepository)
+        {
+            _blogPostRepository = blogPostRepository;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var blogPosts = await _blogPostRepository.GetAllAsync();
+            return View(blogPosts);
+        }
+        [HttpGet]
+        public async Task<IActionResult> CreateOrEdit(Guid Id)
+        {
+            if(Id== Guid.Empty)
+            {
+                return View(new BlogPost());
+            }
+            else
+            {
+                var data = await _blogPostRepository.GetAsync(Id);
+                return View(data);
+            }  
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateOrEdit( Guid Id,BlogPost blogPost)
+        {
+            if (Id == Guid.Empty)
+            {
+                if (ModelState.IsValid)
+                {
+                    var data = await _blogPostRepository.AddAsync(blogPost);
+                    return RedirectToAction("index");
+                }
+            }
+            else
+            {
+                await _blogPostRepository.UpdateAsync(blogPost);
+                return RedirectToAction("Index");
+            }
+            return View(blogPost);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Details(Guid id)
+        {
+            var data = await _blogPostRepository.GetAsync(id);
+            if (data == null)
+            {
+                return NotFound();
+            }
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var data = await _blogPostRepository.DeleteAsync(id);
+            if(data == null) 
+            {           
+                return NotFound();
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BlogNestS/Areas/Admin/Views/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogNestS.Areas.Admin.Views.Controllers
+{
+    [Area("Admin")]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
